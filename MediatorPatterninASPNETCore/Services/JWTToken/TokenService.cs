43da27c7f12a01fcf0345fd25df2830c49e4b6c3
mayoo <mayoozin.developer@gmail.com>
2024-06-09 00:00:07 +0630

    - Added GenerateTokenAsync

--- MediatorPatterninASPNETCore/Services/JWTToken/TokenService.cs
+++ MediatorPatterninASPNETCore/Services/JWTToken/TokenService.cs
@@ -1,8 +1,18 @@
-﻿namespace MediatorPatternInASPNETCore.Services.JWTToken;
+﻿using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace MediatorPatternInASPNETCore.Services.JWTToken;
 
 public class TokenService : ITokenService
 {
     private readonly IConfiguration _configuration;
 
     public TokenService(IConfiguration configuration) => _configuration = configuration;
+
+    public async Task<string> GenerateTokenAsync(string username)
+    {
+        string test = "";
+        return test;
+    }
 }
