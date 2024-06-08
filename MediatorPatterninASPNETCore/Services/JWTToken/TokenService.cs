8af2b920ad08ce72b1a4c4106ade863512e8b915
mayoo <mayoozin.developer@gmail.com>
2024-06-08 23:58:59 +0630

    - Added configuration

--- MediatorPatterninASPNETCore/Services/JWTToken/TokenService.cs
+++ MediatorPatterninASPNETCore/Services/JWTToken/TokenService.cs
@@ -1,6 +1,8 @@
 ﻿namespace MediatorPatternInASPNETCore.Services.JWTToken;
 
-public class TokenService
+public class TokenService : ITokenService
 {
-    
+    private readonly IConfiguration _configuration;
+
+    public TokenService(IConfiguration configuration) => _configuration = configuration;
 }
