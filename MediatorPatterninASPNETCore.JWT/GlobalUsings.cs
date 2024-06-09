382fb9f5c330f39aa160b75d0610b9872c0d53d3
mayoo <mayoozin.developer@gmail.com>
2024-06-09 21:17:46 +0630

    - Added global using

---
+++ MediatorPatterninASPNETCore.JWT/GlobalUsings.cs
@@ -0,0 +1,11 @@
+﻿// Global using directives
+
+global using System.IdentityModel.Tokens.Jwt;
+global using System.Security.Claims;
+global using System.Text;
+global using MediatorPatterninASPNETCore.JWT.Model;
+global using MediatorPatterninASPNETCore.JWT.Services.JWTToken;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.IdentityModel.Tokens;
+global using Microsoft.OpenApi.Models;
