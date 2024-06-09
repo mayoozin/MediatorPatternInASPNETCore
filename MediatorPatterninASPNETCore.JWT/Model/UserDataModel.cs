6d556a0703ab7a3d7013feac1bfb00a7ebd3b957
mayoo <mayoozin.developer@gmail.com>
2024-06-09 21:17:34 +0630

    - Add user data model

---
+++ MediatorPatterninASPNETCore.JWT/Model/UserDataModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MediatorPatterninASPNETCore.JWT.Model;
+
+public class User
+{
+    public string UserName = "mayoo";
+    public string Password = "password";
+}
+
+public class UserDataModel
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+}
