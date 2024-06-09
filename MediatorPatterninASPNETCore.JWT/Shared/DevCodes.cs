ebb48796540669b777cfc674b2c0142a506d8239
mayoo <mayoozin.developer@gmail.com>
2024-06-09 21:17:24 +0630

    - Added Dev code

---
+++ MediatorPatterninASPNETCore.JWT/Shared/DevCodes.cs
@@ -0,0 +1,13 @@
+﻿namespace MediatorPatterninASPNETCore.JWT.Shared;
+
+public static class DevCodes
+{
+    public static string ToHash(this string password)
+    {
+        using var sha256 = System.Security.Cryptography.SHA256.Create();
+        var bytes = Encoding.UTF8.GetBytes(password);
+        var hash = sha256.ComputeHash(bytes);
+        var passwordHash = Convert.ToBase64String(hash);
+        return passwordHash;
+    }
+}
